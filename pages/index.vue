b4064188bad5e783f9765ec325c8153fe2971368
Sigit Prabowo <sgt@prbw.net>
2018-05-28 08:12:14 +0700

    fixing title

--- pages/index.vue
+++ pages/index.vue
@@ -135,7 +135,7 @@ export default {
   },
   head() {
 		return {
-			title: 'Ghiblify',
+			title: 'Ghiblify - Studio Ghibli',
 			meta: [
 				// { charset: 'utf-8' },
 				// { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
